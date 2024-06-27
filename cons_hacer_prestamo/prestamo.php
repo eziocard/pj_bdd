bf34c13fb15c8acda8b9ac2b956a6633648ebd80
ricardo lópez <rjlopezjobs@gmail.com>
2024-06-27 19:23:32 -0400

    actualizacion

--- cons_hacer_prestamo/prestamo.php
+++ cons_hacer_prestamo/prestamo.php
@@ -3,6 +3,24 @@ $db = mysqli_connect("localhost","root","","bdd");
 
 $rut = mysqli_real_escape_string($db, $_POST['rut']);
 $fecha = $_POST['fecha'];
+$id_ejemplar = mysqli_real_escape_string($db, $_POST['id_ejemplar']);
+
+$sql = "call crearPrestamo('$rut','$fecha','$id_ejemplar')";
+
+try{
+    if (mysqli_query($db, $sql)) {
+        echo "El prestamo se agregado correctamente.";
+    } else {
+        echo "Error al llamar al procedimiento almacenado.";
+    }
+} catch (mysqli_sql_exception $e) {
+    if ($e->getCode() == 1062) {
+        echo "Error: El prestamo ya esta registrado";
+    } else {
+        echo "Error SQL: " . $e->getMessage();
+    }
+}
+mysqli_close($db);
 
 
 ?>
--- cons_ingresar_est/ingresar_est.php
+++ cons_ingresar_est/ingresar_est.php
@@ -13,11 +13,19 @@ $carrera = mysqli_real_escape_string($db, $_POST['carrera']);
 
 $sql = "call crearEstudiante('$rut', '$nombre', '$apellido', '$facultad', '$carrera')";
 
-if (mysqli_query($db, $sql)) {
-    echo "Estudiante agregado correctamente.";
-} else {
-    echo "Error al llamar al procedimiento almacenado: " . mysqli_error($db);
-}
 
+try {
+    if (mysqli_query($db, $sql)) {
+        echo "Estudiante agregado correctamente.";
+    } else {
+        echo "Error al llamar al procedimiento almacenado.";
+    }
+} catch (mysqli_sql_exception $e) {
+    if ($e->getCode() == 1062) {
+        echo "Error: El estudiante con rut '$rut' ya está registrado.";
+    } else {
+        echo "Error SQL: " . $e->getMessage();
+    }
+}
 mysqli_close($db);
 ?>
